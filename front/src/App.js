406d124536b6ec4605b888df663b5a8d3e9c1c3f
Tetiana Kurdyukova <tetiana@kurdyukov.info>
2018-11-23 00:33:45 +0200

    added action's folder

--- front/src/App.js
+++ front/src/App.js
@@ -3,35 +3,11 @@ import {Provider, connect}   from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import { GraphQLClient } from 'graphql-request';
 import reducers from './reducers/'
-
-const gql = new GraphQLClient("http://localhost:8000/graphql", { headers: {} });
-const transactionsAll = (`query transactions{
-    transactions{
-          id
-          user
-          date
-          summ
-        }
-      }`);
+import { actionFetch } from './actions/transactions'
 
 let store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
-
 store.subscribe(() => console.log('subscribe', store.getState()))
-
-const actionPending = () => ({type: 'SET_STATUS', status: 'PENDING', payload: [], error: null});
-const actionResolved = payload => ({type: 'SET_STATUS', status: 'RESOLVED', payload, error: null});
-const actionRejected = error => ({type: 'SET_STATUS', status: 'REJECTED', payload: null, error});
-
-function actionFetch() {
-    return dispatch => {
-        dispatch(actionPending());
-        gql.request(transactionsAll)
-                .then(resp => dispatch(actionResolved(resp.transactions)))
-                .catch(error => dispatch(actionRejected(error)));
-    };
-}
 
 class MainBlock extends Component {
     componentDidMount() {
---
+++ front/src/actions/transactions.js
@@ -0,0 +1,24 @@
+import { GraphQLClient } from 'graphql-request';
+
+const gql = new GraphQLClient("http://localhost:8000/graphql", { headers: {} });
+const transactionsAll = (`query transactions{
+    transactions{
+          id
+          user
+          date
+          summ
+        }
+      }`);
+
+const actionPending = () => ({type: 'SET_STATUS', status: 'PENDING', payload: [], error: null});
+const actionResolved = payload => ({type: 'SET_STATUS', status: 'RESOLVED', payload, error: null});
+const actionRejected = error => ({type: 'SET_STATUS', status: 'REJECTED', payload: null, error});
+
+export function actionFetch() {
+    return dispatch => {
+        dispatch(actionPending());
+        gql.request(transactionsAll)
+                .then(resp => dispatch(actionResolved(resp.transactions)))
+                .catch(error => dispatch(actionRejected(error)));
+    };
+}
